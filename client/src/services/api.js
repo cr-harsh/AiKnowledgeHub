17134e69b5a81369b61835180baba7a11bd3273a
cr-harsh <harshbhuteja56@gmail.com>
2026-07-07 05:21:31 +0530

    last one

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 // Express API URL
-const API_URL = import.meta.env.VITE_API_URL || 'http://localhost:5000/api';
+const API_URL = import.meta.env.VITE_API_URL || "https://aiknowledgehub-server.onrender.com/api";
 
 const api = axios.create({
   baseURL: API_URL,
